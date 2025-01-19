870b917f71cfd67e3cc460e35242febf124bb4da
PrincePatch <166150301+PrincePatch@users.noreply.github.com>
2025-01-19 21:40:32 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+ #!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='GameOfLife-FlorianThibault-TD6',
+    version='1.0',
+    author='PrincePatch',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
