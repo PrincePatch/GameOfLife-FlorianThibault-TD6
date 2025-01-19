c353f8a3b7169b730bbd28d9207858b750aa6fdc
Your Name <your_email@example.com>
2025-01-19 22:05:15 +0100

    Ajout de la documentation du projet

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  #!/usr/bin/env python
 from distutils.core import setup
 
@@ -8,15 +7,3 @@ setup(
     author='PrincePatch',
     license='MIT',
     long_description=open('README.md').read(),
-=======
- #!/usr/bin/env python
-from distutils.core import setup
-
-setup(
-    name='GameOfLife-FlorianThibault-TD6',
-    version='1.0',
-    author='PrincePatch',
-    license='MIT',
-    long_description=open('README.md').read(),
->>>>>>> 870b917f71cfd67e3cc460e35242febf124bb4da
-)
